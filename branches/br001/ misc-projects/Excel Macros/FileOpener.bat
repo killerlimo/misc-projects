1af94f5c6c8e2cb9206de7848ef88ae99880b040
killerlimo@gmail.com <killerlimo@gmail.com@c72980a3-9f7f-a2f4-ceb6-bfc96dacb6cc>
2013-08-14 12:37:58 +0000

    FileOpener.bat: Not working, problem with '>'.

--- branches/br001/ misc-projects/Excel Macros/FileOpener.bat
+++ branches/br001/ misc-projects/Excel Macros/FileOpener.bat
@@ -1,23 +1,33 @@
 @echo off
-echo Number: %1%
-echo Type: %2%
+REM Determine whether network is present
 
-set IndexPath=" c:\Drgstate\PartsCurrentIndex.txt
+set NetDataPath=\\atle.bombardier.com\data\uk\pl\dos\Drgstate\
+set ResultPath= ^> c:\windows\temp\FileOpenResult.txt
 
+if not exist "%NetDataPath%\nul" set NetDataPath=c:\Drgstate\
+
+	
+REM Determine whether material or drawing
 if "%2%"=="material" (
-	echo It is a Material
-	find /i "%1% %%IndexPath%% >  Result.txt
+	set IndexPath=" %NetDataPath%PartsCurrentIndex.txt
 ) else (
-	echo It is a Drawing
+	set IndexPath=" %NetDataPath%CurrentIndex.txt
 )
 
-echo Results
-for /F "tokens=*"  %%i IN ('findstr /v "INDEX" Result.txt') DO (
-	"%%i"
+REM Search for a match in the index
+set FindFiles=%IndexPath%% ^> %ResultPath%
+echo %IndexPath%%
+echo %ResultPath%
+echo %FindFiles%
+
+rem find /i "%1% %%FindFiles%%
+
+REM Stop at first line containing a path and open the file
+for /F "tokens=*"  %%i IN ('findstr /v "INDEX" %ResultPath%') DO (
+	rem"%%i"
 	goto :Finish
 )
 
 :Finish
 
-echo Finished
 
