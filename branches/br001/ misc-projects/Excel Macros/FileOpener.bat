640159f898938a62cf95a4d508124892ed93e357
KillerLimo@gmail.com <KillerLimo@gmail.com@c72980a3-9f7f-a2f4-ceb6-bfc96dacb6cc>
2013-08-14 20:36:05 +0000

    FileOpener.bat: Nearer working.

--- branches/br001/ misc-projects/Excel Macros/FileOpener.bat
+++ branches/br001/ misc-projects/Excel Macros/FileOpener.bat
@@ -4,41 +4,32 @@ Setlocal EnableDelayedExpansion
 
 REM Determine whether network is present
 
-set "NetDataPath=\\atle.bombardier.com\data\uk\pl\dos\Drgstate\"
-set "ResultPath= c:\windows\temp\FileOpenResult.txt"
+set NetDataPath=\\atle.bombardier.com\data\uk\pl\dos\Drgstate\
+set ResultPath=c:\windows\temp\FileOpenResult.txt
 
 if not exist %NetDataPath% (
 	set NetDataPath=c:\Drgstate\
-	REM echo No network
+	echo No network
 ) else (
 	echo Network
 )
 
 REM Determine whether material or drawing
-if "%2%"=="material" (
-	set IndexPath=" %NetDataPath%PartsCurrentIndex.txt
+if "%2%"=="Material" (
+	set IndexPath=%NetDataPath%PartsCurrentIndex.txt
 	echo Material
 ) else (
-	set IndexPath=" %NetDataPath%CurrentIndex.txt
+	set IndexPath=%NetDataPath%CurrentIndex.txt
 	echo Drawing
 )
 
 REM Search for a match in the index
 set "Redirect=>"
-set "FindFiles=%IndexPath%%%Redirect%%ResultPath%"
+set FindFiles="%IndexPath% %Redirect% %ResultPath%"
 echo Index:%IndexPath%%
 echo Result:%ResultPath%
-echo Find:!FindFiles!
+echo Find:%FindFiles%
 
-
-rem find /i "%1% %%FindFiles%%
-
-REM Stop at first line containing a path and open the file
-for /F "tokens=*"  %%i IN ('findstr /v "INDEX" %ResultPath%') DO (
-	rem"%%i"
-	goto :Finish
-)
-
-:Finish
+find /i %1% %FindFiles%
 
 
