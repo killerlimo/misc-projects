319ea67dadf117ece2a5c9f9d23c678a1e5e5965
killerlimo@gmail.com <killerlimo@gmail.com@c72980a3-9f7f-a2f4-ceb6-bfc96dacb6cc>
2013-08-14 16:46:55 +0000

    FileOpener.bat: Network detection and material detection working. Find not working.

--- branches/br001/ misc-projects/Excel Macros/FileOpener.bat
+++ branches/br001/ misc-projects/Excel Macros/FileOpener.bat
@@ -1,24 +1,35 @@
 @echo off
+
+Setlocal EnableDelayedExpansion
+
 REM Determine whether network is present
 
-set NetDataPath=\\atle.bombardier.com\data\uk\pl\dos\Drgstate\
-set ResultPath= ^> c:\windows\temp\FileOpenResult.txt
+set "NetDataPath=\\atle.bombardier.com\data\uk\pl\dos\Drgstate\"
+set "ResultPath= c:\windows\temp\FileOpenResult.txt"
 
-if not exist "%NetDataPath%\nul" set NetDataPath=c:\Drgstate\
+if not exist %NetDataPath% (
+	set NetDataPath=c:\Drgstate\
+	REM echo No network
+) else (
+	echo Network
+)
 
-	
 REM Determine whether material or drawing
 if "%2%"=="material" (
 	set IndexPath=" %NetDataPath%PartsCurrentIndex.txt
+	echo Material
 ) else (
 	set IndexPath=" %NetDataPath%CurrentIndex.txt
+	echo Drawing
 )
 
 REM Search for a match in the index
-set FindFiles=%IndexPath%% ^> %ResultPath%
-echo %IndexPath%%
-echo %ResultPath%
-echo %FindFiles%
+set "Redirect=>"
+set "FindFiles=%IndexPath%%%Redirect%%ResultPath%"
+echo Index:%IndexPath%%
+echo Result:%ResultPath%
+echo Find:!FindFiles!
+
 
 rem find /i "%1% %%FindFiles%%
 
